a096a7a88ec588e6d045a4711bdf9d2f67156404
Nitin Dogra <90884243+NitinDogra007@users.noreply.github.com>
2025-05-15 16:12:42 +1000

    connect backend files to one another

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import clientRoutes from './routes/clientRoute.js';
 
 const app = express();
 const port = 3000;
@@ -7,9 +8,7 @@ const port = 3000;
 app.use(cors()); // Enable CORS
 app.use(express.json()); // Middleware to parse JSON
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello Backend Test</h1>');
-});
+app.use('/api', clientRoutes);
 
 app.listen(port, () => {
 	console.log('listening to port', port);
